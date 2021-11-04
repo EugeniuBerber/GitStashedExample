5890c091b2d0b6650fa3cac239eaea6347350384
EugeniuBerber <75701425+EugeniuBerber@users.noreply.github.com>
2021-11-03 18:18:08 -0600

    Lincoln Aviator White

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Car car = new Car();
-        car.make= "Zaibatsu";
+        car.make= "Lincoln";
+        car.model="Aviator";
+        car.color="White";
+        car.price=110000;
+        System.out.println(car.make+" "+car.model);
     }
 }
