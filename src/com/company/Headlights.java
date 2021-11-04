6c025b229bdb94a9f97f852ccf29fb72ff44b4e5
EugeniuBerber <75701425+EugeniuBerber@users.noreply.github.com>
2021-11-03 19:23:39 -0600

    added headlight class

---
+++ src/com/company/Headlights.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Headlights {
+    public String color;
+    public int size;
+    public String type;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Car car = new Car();
 
-        car.make= "Mazda";
+        car.make = "Mazda";
         car.model = "CX9";
-        car.color ="Red";
+        car.color = "Red";
         car.price = 41000;
-        
-        
+
+        Headlights hl = new Headlights();
+        hl.color = "yellow";
+        hl.size = 12;
+        hl.type="LED";
+
+
     }
 }
